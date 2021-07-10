aba8672d7c0706745e1a42c900357aa16677398c
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-09 21:58:25 -0400

    Setup routing and fetch data to display in '/shop'

---
+++ src/About.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Shop.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import {Link} from 'react-router-dom'
+
+const Shop = () => {
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  const [items, setItems] = useState([])
+
+  const fetchItems = async () => {
+    const data = await fetch('https://fakestoreapi.com/products?limit=50')
+    
+    const items = await data.json()
+    console.log(items)
+    setItems(items)
+  }
+
+  return (
+    <div>
+      {items.map(item => (
+        <h6 key={item.id}>
+          <Link to={`/shop/${item.id}`}>{item.title}</Link>
+          
+        </h6>
+      ))}
+    </div>
+  );
+}
+
+export default Shop;
